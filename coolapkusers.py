c4a32019ef718de66457beddd201a119f53556c0
NewConn <n1141359233@gmail.com>
2017-05-22 20:47:57 +0800

    异常捕获，拆分函数
    增加下载页面的异常捕获和编码处理
    把整个爬虫拆分为函数，易于维护

--- coolapkusers.py
+++ coolapkusers.py
@@ -1,15 +1,24 @@
-#coding=utf-8
-import re
 import requests
+import re
+import os
 
-for i in range(784579, 790156):#2017年1月24日 10:13:08
-    raw = requests.get('http://coolapk.com/u/'+str(i)).text
+def getHTML(url):
+    try:
+        r=requests.get(url)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        print("getHTML")
+
+
+def parsePage(raw):
     a = re.findall('setTimeout', raw)
     if not a == []:
         with open('reserved.txt', 'a') as reserved:
-            reserved.write(str(i)+'\n')
+            reserved.write(str(i) + '\n')
         continue
-    
+
     b = re.findall('alt="(\S+)"', raw)
     if not b == []:
         dt = re.findall(r'<strong>(\d+)</strong>动态', raw)[0]
@@ -18,13 +27,27 @@ for i in range(784579, 790156):#2017年1月24日 10:13:08
         fs = re.findall(r'<strong>(\d+)</strong>好友关注', raw)[0]
         try:
             with open('users.csv', 'a') as users:
-                users.write(str(i)+','+b[0]+','+dt+','+yy+','+fx+','+fs+'\n')
+                users.write(str(i) + ',' + b[0] + ',' + dt + ',' + yy + ',' + fx + ',' + fs + '\n')
         except UnicodeEncodeError:
             with open('unicode.txt', 'a') as un:
-                un.write(str(i)+'\n')
+                un.write(str(i) + '\n')
         except:
             with open('others.txt', 'a') as others:
-                others.write(str(i)+'\n') 
+                others.write(str(i) + '\n')
     else:
         with open('others.txt', 'a') as others:
-            others.write(str(i)+'\n') 
+            others.write(str(i) + '\n')
+
+'''def writeUserList(a,b):
+    tplt="{:4}\t{:8}\t{:16}\n"
+'''
+def main():
+    for i in range(784579, 790156):
+        start_url = ('http://coolapk.com/u/' + str(i)).text
+        raw = getHTML(start_url)
+        parsePage(raw)
+    users.close()
+    un.close()
+    others.close()
+
+main()
